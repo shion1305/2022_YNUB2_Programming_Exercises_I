c32151341f0e76abd5186c388443cf5a5a12f577
Shion Ichikawa <shion1305@gmail.com>
2022-05-19 23:22:57 +0900

    [FIX] fix minor bugs and add some comments

--- Lesson5/2164027_b2.c
+++ Lesson5/2164027_b2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//EOFとは別に自分で管理用のEnd Of Inputを定める
 #define EOI -12345
 
 void count(int *x, int *c);
@@ -9,7 +10,9 @@ int main() {
     int input;
     int array[100];
     int position = 0;
+    //まずarrayに格納する。
     while (scanf("%d", &input) != EOF) {
+        //無効な値の検知
         if (0 > input || input > 9) {
             printf("無効な範囲です。\n");
             continue;
@@ -18,7 +21,10 @@ int main() {
     }
     array[position] = EOI;
     int c[10] = {0};
+
+    //arrayをカウントさせる。
     count(array, c);
+    //適切なフォーマット尾での出力
     for (int i = 0; i < 10; ++i) {
         printf("%d: %d\n", i, c[i]);
     }
@@ -26,6 +32,7 @@ int main() {
 
 //assert all elements are 0~9
 void count(int *x, int *c) {
+    //ポインタを活用してcの0~9番地に出力する
     do {
         *(c + *x) += 1;
     } while (*(x++) != EOI);
--- Lesson5/2164027_b4.c
+++ Lesson5/2164027_b4.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
+//内積を計算する関数のプロトタイプ宣言
 int innerProduct(int *v1, int *v2);
 
+//入力を受け取る関数のプロトタイプ宣言
+void ask_for_input(int *vector);
+
 int main() {
-    int vector1[3] = {1, 1, 1};
-    int vector2[3] = {2, 2, 2};
-    printf("%d", innerProduct(&vector1[0], &vector2[0]));
+    int vector1[3];
+    int vector2[3];
+    //入力を格納するベクトルの用意
+    //それぞれのベクトルの入力受付
+    printf("ベクトル1を入力してください。\n");
+    ask_for_input(vector1);
+    printf("ベクトル2を入力してください。\n");
+    ask_for_input(vector2);
+    //内積の計算
+    printf("内積: %d", innerProduct(vector1, vector2));
+}
+
+void ask_for_input(int *vector) {
+    for (int i = 0; i < 3; ++i) {
+        //それぞれ受け取り、ポインタによって格納
+        scanf("%d", vector + i);
+    }
 }
 
 int innerProduct(int v1[], int v2[]) {
+    //問題の指定によりポインタを使わずに計算する
     return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 }
