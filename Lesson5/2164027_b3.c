650b5193195786f89b70569730c82130840c1a84
Shion Ichikawa <shion1305@gmail.com>
2022-05-25 19:19:23 +0900

    [FIX] requirement had not met in 2164027_b3.c

--- Lesson5/2164027_b3.c
+++ Lesson5/2164027_b3.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 
-//入力用
+//入力用引数に与えられた参照型配列に代入操作をする。
 void input_mat(double target[2][2]);
 
-//出力用
-void print_mat(double target[2][2], double inverse);
+//出力用。引数に与えられた変数をプリントする。
+void print_mat(double target[2][2]);
 
-//逆行列を求める用の、行列式の値を計算する関数
-double inverse(double target[2][2]);
+//逆行列を求める関数
+double inverse(double input[2][2], double output[2][2]);
 
 int main() {
     double mat[2][2];
-    //入力を求める
+    //input_matで入力を求める
     input_mat(mat);
-    //inverseで計算した結果を出力用の関数に渡す
-    print_mat(mat, inverse(mat));
+    //inverseで逆行列を求め、inversedに格納する。
+    //また、行列式の値をdに格納し、d==0で逆行列は存在しないので、
+    //存在しない旨の表示を出す。
+    double inversed[2][2];
+    double d = inverse(mat, inversed);
+    printf("行列\n");
+    print_mat(mat);
+    if (d == 0.0) {
+        printf("の逆行列は存在しない。");
+        return 0;
+    }
+    printf("の逆行列は\n");
+    print_mat(inversed);
 }
 
 
 void input_mat(double target[2][2]) {
     printf("2 x 2 の行列の要素の値を入力してください\n");
+
     //それぞれの要素に対して値を挿入していく。
     for (int i = 0; i < 2; ++i) {
         for (int j = 0; j < 2; ++j) {
@@ -29,25 +41,25 @@ void input_mat(double target[2][2]) {
     }
 }
 
-void print_mat(double target[2][2], double inverse) {
-    printf("行列\n");
-    for (int i = 0; i < 2; ++i) {
-        //指定したフォーマットでの出力。
-        printf("%2.5lf  %2.5lf\n", target[i][0], target[i][1]);
-    }
-    //逆行列が存在しない時(つまり inverse==0)
-    if (inverse == 0.0) {
-        printf("の逆行列は存在しない");
-        return;
-    }
-    //この後は逆行列が存在する場合にのみ実行される。
-    //逆行列の定義に従ってそれぞれ計算。
-    printf("の逆行列は\n");
-    printf("%2.5lf  %2.5lf\n", *(target[1] + 1) / inverse, -1 * *(target[0] + 1) / inverse);
-    printf("%2.5lf  %2.5lf\n", -1 * *target[1] / inverse, *target[0] / inverse);
+void print_mat(double target[2][2]) {
+    printf("%2.5lf  %2.5lf\n", target[0][0], target[0][1]);
+    printf("%2.5lf  %2.5lf\n", target[1][0], target[1][1]);
 }
 
-double inverse(double target[2][2]) {
+double determinant(double target[2][2]) {
     //ポインタの性質を用いて計算(この問題では使っても大丈夫でしたよね..?)
     return *target[0] * *(target[1] + 1) - *(target[0] + 1) * *target[1];
+}
+
+double inverse(double input[2][2], double output[2][2]) {
+    //2次正方行列の逆行列の式
+    double d = determinant(input);
+    //行列式が0ならばもちろん逆行列は存在しないので、要件に従って0を返す。
+    if (d == 0) return 0;
+    //行列式が存在するとき、2次正方行列の定義に従ってそれぞれの要素を計算する
+    output[0][0] = *(input[1] + 1) / d;
+    output[0][1] = -1 * *(input[0] + 1) / d;
+    output[1][0] = -1 * *input[1] / d;
+    output[1][1] = *input[0] / d;
+    return d;
 }
