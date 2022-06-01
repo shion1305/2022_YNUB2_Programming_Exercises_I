88b0c7e045cc4d912bc72d1f756bd58bac7dee04
Shion Ichikawa <shion1305@gmail.com>
2022-06-01 18:50:58 +0900

    [CHANGE] add comments in 2164027_b2.c

--- Lesson7/2164027_b2.c
+++ Lesson7/2164027_b2.c
@@ -11,18 +11,27 @@ int main(int argc, char *argv[]) {
         printf("Arguments are insufficient.\n");
         return 0;
     }
+    //コマンド引数から読み込んでそれぞれのファイルを開く処理をする。
     fp = fopen(argv[1], "r");
     fp1 = fopen(argv[2], "r");
+    //これから読み込んで検証する際に読み込んだcharを一時保存するための変数。
     char c1, c2;
+    //比較した後に結果を保存するための変数。
     int result = 1;
     do {
+        //それぞれ一時変数に1文字ずつcharを取り込んでいく。
         c1 = fgetc(fp);
         c2 = fgetc(fp1);
         if (c1 != c2) {
             result = 0;
             break;
         }
+        //EOFになるまで続行する。
     } while (c1 != EOF);
+
+    // while文を抜けてこのif文が実行されるのは
+    // 結果が分かってresultで分かっているとき。
+    //よって最後はresultの値で出力内容を条件分岐しよう。
     if (result) {
         printf("2つのファイルは同一です。");
     } else {
