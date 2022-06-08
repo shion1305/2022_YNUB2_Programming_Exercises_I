ef097b4fcd393121d427d767e6ba5e766f71c026
Shion Ichikawa <shion1305@gmail.com>
2022-06-08 21:08:46 +0900

    [DOC] add copyright

--- Lesson8/2164027_b1.c
+++ Lesson8/2164027_b1.c
@@ -1,3 +1,12 @@
+// Copyright (c) 2022 Shion Ichikawa.
+// <==============================FOR VIEWERS==============================>
+//       If you find software that doesn’t have a license, that generally means
+//  you have no permission from the creators of the software to use, modify, or
+// share the software. Although a code host such as GitHub may allow you to
+// view and fork the code, this does not imply that you are permitted to use,
+// modify, or share the software for any purpose.
+// <=======================================================================>
+
 #include <stdio.h>
 
 typedef struct {
