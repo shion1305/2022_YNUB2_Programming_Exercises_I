6439e04a97056da2e3a98ff77d65be36d9cc84f4
Shion Ichikawa <shion1305@gmail.com>
2022-07-05 04:02:43 +0900

    [FIX] requirement had not met. make output to external file.

--- Lesson11/2164027_b3.c
+++ Lesson11/2164027_b3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define N 10
 int data[N][N] = {
@@ -14,28 +15,28 @@ int data[N][N] = {
         {8, 8, 8, 8, 8, 8, 8, 8, 9, 8}};
 
 //結果を出力する関数。
-void printR(int d[N][N], int step) {
-    printf("%2dで到着\n", step);
+void printR(int d[N][N], int step, FILE *out) {
+    fprintf(out, "%2dで到着\n", step);
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             switch (d[i][j]) {
                 case 0:
-                    printf("□");
+                    fprintf(out, "□");
                     break;
                 case 8:
-                    printf("■");
+                    fprintf(out, "■");
                     break;
                 default:
-                    printf("〇");
+                    fprintf(out, "〇");
             }
         }
-        printf("\n");
+        fprintf(out, "\n");
     }
-    printf("\n");
+    fprintf(out, "\n");
 }
 
 //上下左右を確認し、もし道が見つかれば次へ進む。
-void search(int x, int y, int d[N][N], int step) {
+void search(int x, int y, int d[N][N], int step, FILE *out) {
     //道があれば(0,1)その道をマーク。なければ関数を探索を終了する。
     switch (d[x][y]) {
         case 0:
@@ -43,26 +44,26 @@ void search(int x, int y, int d[N][N], int step) {
         case 1:
             break;
         case 9:
-            printR(d, step);
+            printR(d, step, out);
         default:
             return;
     }
     step++;
     //上下左右に再帰的に探索。
-    if (x > 0) search(x - 1, y, d, step);
-    if (y > 0) search(x, y - 1, d, step);
-    if (x < N - 1) search(x + 1, y, d, step);
-    if (y < N - 1) search(x, y + 1, d, step);
+    if (x > 0) search(x - 1, y, d, step, out);
+    if (y > 0) search(x, y - 1, d, step, out);
+    if (x < N - 1) search(x + 1, y, d, step, out);
+    if (y < N - 1) search(x, y + 1, d, step, out);
     if (d[x][y] == 2) d[x][y] = 0;
 }
 
 //開始地点を見つけスタートさせる。
-void start(int d[N][N]) {
+void start(int d[N][N], FILE *out) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             if (d[i][j] == 1) {
                 d[i][j] = 0;
-                search(i, j, d, 0);
+                search(i, j, d, 0, out);
                 return;
             }
         }
@@ -70,6 +71,12 @@ void start(int d[N][N]) {
 }
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("ERROR, NO ARGUMENTS");
+        exit(-1);
+    }
+    FILE *out = fopen(argv[1], "w");
     //処理の開始
-    start(data);
+    start(data, out);
+    printf("Completed. Output: %s.", argv[1]);
 }
