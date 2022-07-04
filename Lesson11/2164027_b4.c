682b77ce77be473afc56ee6cdffb99f6d1c59b64
Shion Ichikawa <shion1305@gmail.com>
2022-07-05 04:38:15 +0900

    [CHORE] remove unused variable "sum"

--- Lesson11/2164027_b4.c
+++ Lesson11/2164027_b4.c
@@ -36,8 +36,8 @@ void printR(int d[N][N], int step, FILE *out) {
 }
 
 //上下左右を確認し、もし道が見つかれば次へ進む。解の数を返す。
-int search(int x, int y, int d[N][N], int step, FILE *out, int sum) {
-    int sum_r = sum;
+int search(int x, int y, int d[N][N], int step, FILE *out) {
+    int sum_r = 0;
     //道があれば(0,1)その道をマーク。なければ関数を探索を終了する。
     switch (d[x][y]) {
         case 0:
@@ -52,10 +52,10 @@ int search(int x, int y, int d[N][N], int step, FILE *out, int sum) {
     }
     step++;
     //解の数を記録しながら上下左右に再帰的に探索。
-    if (x > 0) sum_r += search(x - 1, y, d, step, out, sum);
-    if (y > 0) sum_r += search(x, y - 1, d, step, out, sum);
-    if (x < N - 1) sum_r += search(x + 1, y, d, step, out, sum);
-    if (y < N - 1) sum_r += search(x, y + 1, d, step, out, sum);
+    if (x > 0) sum_r += search(x - 1, y, d, step, out);
+    if (y > 0) sum_r += search(x, y - 1, d, step, out);
+    if (x < N - 1) sum_r += search(x + 1, y, d, step, out);
+    if (y < N - 1) sum_r += search(x, y + 1, d, step, out);
     if (d[x][y] == 2) d[x][y] = 0;
     return sum_r;
 }
@@ -66,13 +66,14 @@ int start(int d[N][N], FILE *out) {
         for (int j = 0; j < N; ++j) {
             if (d[i][j] == 1) {
                 d[i][j] = 0;
-                return search(i, j, d, 0, out, 0);
+                return search(i, j, d, 0, out);
             }
         }
     }
 }
 
 int main(int argc, char *argv[]) {
+    //引数がない時にエラー
     if (argc < 2) {
         printf("ERROR, NO ARGUMENTS");
         exit(-1);
