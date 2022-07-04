4ae7247eae4a48cb42992a29b2b7cd898dea46b3
Shion Ichikawa <shion1305@gmail.com>
2022-07-05 02:43:38 +0900

    [DOC] add comments in 2164027_b3.c for Lesson11

--- Lesson11/2164027_b3.c
+++ Lesson11/2164027_b3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define N 10
 int data[N][N] = {
@@ -14,6 +13,7 @@ int data[N][N] = {
         {8, 8, 8, 0, 0, 0, 0, 8, 0, 8},
         {8, 8, 8, 8, 8, 8, 8, 8, 9, 8}};
 
+//結果を出力する関数。
 void printR(int d[N][N], int step) {
     printf("%2dで到着\n", step);
     for (int i = 0; i < N; ++i) {
@@ -34,10 +34,13 @@ void printR(int d[N][N], int step) {
     printf("\n");
 }
 
+//上下左右を確認し、もし道が見つかれば次へ進む。
 void search(int x, int y, int d[N][N], int step) {
+    //道があれば(0,1)その道をマーク。なければ関数を探索を終了する。
     switch (d[x][y]) {
         case 0:
             data[x][y] = 2;
+        case 1:
             break;
         case 9:
             printR(d, step);
@@ -45,6 +48,7 @@ void search(int x, int y, int d[N][N], int step) {
             return;
     }
     step++;
+    //上下左右に再帰的に探索。
     if (x > 0) search(x - 1, y, d, step);
     if (y > 0) search(x, y - 1, d, step);
     if (x < N - 1) search(x + 1, y, d, step);
@@ -52,7 +56,20 @@ void search(int x, int y, int d[N][N], int step) {
     if (d[x][y] == 2) d[x][y] = 0;
 }
 
+//開始地点を見つけスタートさせる。
+void start(int d[N][N]) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (d[i][j] == 1) {
+                d[i][j] = 0;
+                search(i, j, d, 0);
+                return;
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
-    data[1][1] = 0;
-    search(1, 1, data, 0);
+    //処理の開始
+    start(data);
 }
